5be6cfe4149306956c72e9d938e89bb66a430cf7
charlesassoi <charlesassoi07@gmail.com>
2026-09-24 07:32:59 +0000

    new

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+pip install -q langgrap langchain langchain_openai 
+
 from langchain_openai import ChatOpenAi
 from langgraph.Graph import StateGraph
 
 def ask_question(State):
-    llm=ChatOpenAi(model="gpt-4o-mini")
-    
+    llm=ChatOpenAi(model="gpt-4o-mini",api_key="your_api_key_here")
+    response=llm.invoke("give me one motivational quote")
+    return{"output":response.content}
+
+
+graph=StateGraph(dict)
+graph.add_node("ask",ask_question)
+graph.set_entry_point("ask")
+graph.set_finish_point("ask")
+
+app=graph.compile()
+result=app.invoke({})
+print(result["output"])
